c6ca5d6576e46671969c33984ff0f23b654502b9
AiXin18 <91781056+AiXin18@users.noreply.github.com>
2022-05-16 16:17:20 +0800

    small push also
    able to jump from dashboard to mycats

--- AppDev/app/src/main/java/com/example/tiktokappdev/MainActivity.java
+++ AppDev/app/src/main/java/com/example/tiktokappdev/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.tiktokappdev;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private CardView cardview_ourcats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        cardview_ourcats = findViewById(R.id.cardview_ourcats);
+        cardview_ourcats.setOnClickListener(this);
+
     }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.cardview_ourcats:
+                startActivity(new Intent(getApplicationContext(), CatsActivity.class));
+                break;
+
+        }
+
+
+    }
+
+
 }
