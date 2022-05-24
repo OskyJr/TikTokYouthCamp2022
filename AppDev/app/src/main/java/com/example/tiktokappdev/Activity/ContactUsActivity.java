649c4b6f1cbbe98711e072fb1eaa575e458be103
AiXin18 <91781056+AiXin18@users.noreply.github.com>
2022-05-25 02:21:04 +0800

    UI small push
    added the pages for some parts of the appointment and contact us page

---
+++ AppDev/app/src/main/java/com/example/tiktokappdev/Activity/ContactUsActivity.java
@@ -0,0 +1,16 @@
+package com.example.tiktokappdev.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tiktokappdev.R;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contactus);
+    }
+}
--- AppDev/app/src/main/java/com/example/tiktokappdev/Activity/DashboardActivity.java
+++ AppDev/app/src/main/java/com/example/tiktokappdev/Activity/DashboardActivity.java
@@ -6,21 +6,69 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tiktokappdev.Activity.CatsActivity;
+import com.example.tiktokappdev.DataManagers.UserDetailsDataManager;
+import com.example.tiktokappdev.DataModels.UserDetailsDataModel;
+import com.example.tiktokappdev.MainActivity;
 import com.example.tiktokappdev.R;
+import com.example.tiktokappdev.SessionManager.SessionManager;
 
 public class DashboardActivity extends AppCompatActivity implements View.OnClickListener  {
 
     private CardView cardview_ourcats;
+    private CardView cardview_myreservation;
+    private CardView cardview_aboutandcontactus;
+
+    private Button btn_logout;
+
+    String UserName;
+    String UserEmail;
+    UserDetailsDataManager UDDM;
+    SessionManager sessionManager;
+    private TextView tv_LoginSessionUserID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboardactivity);
 
+
+//        sessionManager = new SessionManager(getApplicationContext());
+//        tv_LoginSessionUserID.setText(sessionManager.getUserID().toString());  // set name of user who login
+//        UDDM = new UserDetailsDataManager();
+//        final UserDetailsDataModel[] userDetailsDataModel = {null};
+//        userDetailsDataModel[0] = UDDM.GetUserProfileDetails(sessionManager.getUserID().toString());
+//        UserName = userDetailsDataModel[0].getName().toString();
+//        UserEmail = userDetailsDataModel[0].getEmail().toString();
+
+
+
+
+        btn_logout = findViewById(R.id.btn_logout);
+
         cardview_ourcats = findViewById(R.id.cardview_ourcats);
         cardview_ourcats.setOnClickListener(this);
+
+        cardview_myreservation = findViewById(R.id.cardview_myreservation);
+        cardview_myreservation.setOnClickListener(this);
+
+        cardview_aboutandcontactus = findViewById(R.id.cardview_aboutandcontactus);
+        cardview_aboutandcontactus.setOnClickListener(this);
+
+
+        btn_logout = findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                sessionManager.setLogin(false);
+                sessionManager.setUserID("");
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+            }
+        });
 
     }
 
@@ -31,6 +79,14 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
 
             case R.id.cardview_ourcats:
                 startActivity(new Intent(getApplicationContext(), CatsActivity.class));
+                break;
+
+            case R.id.cardview_myreservation:
+                startActivity(new Intent(getApplicationContext(), AppointmentActivity.class));
+                break;
+
+            case R.id.cardview_aboutandcontactus:
+                startActivity(new Intent(getApplicationContext(), ContactUsActivity.class));
                 break;
 
         }
