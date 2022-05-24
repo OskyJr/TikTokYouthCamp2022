ff170db714f19e4640fd7ab530e0ece879dab9fe
OskyJr <49112599+OskyJr@users.noreply.github.com>
2022-05-24 21:21:27 +0800

    wip for menu page

--- AppDev/app/src/main/java/com/example/tiktokappdev/Activity/DashboardActivity.java
+++ AppDev/app/src/main/java/com/example/tiktokappdev/Activity/DashboardActivity.java
@@ -5,14 +5,17 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 import com.example.tiktokappdev.Activity.CatsActivity;
+import com.example.tiktokappdev.Activity.MenuActivity;
 import com.example.tiktokappdev.R;
 
 public class DashboardActivity extends AppCompatActivity implements View.OnClickListener  {
 
     private CardView cardview_ourcats;
+    private CardView cardview_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,8 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         cardview_ourcats = findViewById(R.id.cardview_ourcats);
         cardview_ourcats.setOnClickListener(this);
 
+        cardview_menu = findViewById(R.id.cardview_menu);
+        cardview_menu.setOnClickListener(this);
     }
 
     @Override
@@ -31,6 +36,9 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
 
             case R.id.cardview_ourcats:
                 startActivity(new Intent(getApplicationContext(), CatsActivity.class));
+                break;
+            case R.id.cardview_menu:
+                startActivity(new Intent(getApplicationContext(), MenuActivity.class));
                 break;
 
         }
---
+++ AppDev/app/src/main/java/com/example/tiktokappdev/Activity/MenuActivity.java
@@ -0,0 +1,49 @@
+package com.example.tiktokappdev.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tiktokappdev.DataModels.MyCatsDataModel;
+import com.example.tiktokappdev.Fragments.MyCatsDetailedFragment;
+import com.example.tiktokappdev.Fragments.MyCatsMasterFragment;
+import com.example.tiktokappdev.R;
+
+public class MenuActivity extends AppCompatActivity implements MyCatsMasterFragment.CatsSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menuactivity);
+
+        ShowCatMasterFragment();
+    }
+
+    protected void ShowCatMasterFragment()
+    {
+        MyCatsMasterFragment newFragmentToShow = new MyCatsMasterFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout_mainMyCat, newFragmentToShow).commit();
+    }
+
+    protected void ShowCatDetailFragmentOverCatMasterFragment(String selectedCatID)
+    {
+        MyCatsDetailedFragment newFragmentToShow = new MyCatsDetailedFragment();
+        Bundle params = new Bundle();
+        params.putString("CatID", selectedCatID);
+        newFragmentToShow.setArguments(params);
+        getSupportFragmentManager()
+                .beginTransaction()
+                // .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right)   // animation
+                .replace(R.id.frameLayout_mainMyCat, newFragmentToShow)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void onCatSelected(MyCatsDataModel cats) {
+
+        ShowCatDetailFragmentOverCatMasterFragment(cats.getCatID());
+
+    }
+
+}
