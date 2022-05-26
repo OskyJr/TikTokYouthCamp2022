6da7ccadd761dbf767c7bc21bc4b21095052ccc5
OskyJr <49112599+OskyJr@users.noreply.github.com>
2022-05-27 00:29:40 +0800

    fixed menu page access issue

--- AppDev/app/src/main/java/com/example/tiktokappdev/Activity/MenuActivity.java
+++ AppDev/app/src/main/java/com/example/tiktokappdev/Activity/MenuActivity.java
@@ -9,41 +9,11 @@ import com.example.tiktokappdev.Fragments.MyCatsDetailedFragment;
 import com.example.tiktokappdev.Fragments.MyCatsMasterFragment;
 import com.example.tiktokappdev.R;
 
-public class MenuActivity extends AppCompatActivity implements MyCatsMasterFragment.CatsSelectedListener {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menuactivity);
-
-        ShowCatMasterFragment();
     }
-
-    protected void ShowCatMasterFragment()
-    {
-        MyCatsMasterFragment newFragmentToShow = new MyCatsMasterFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout_mainMyCat, newFragmentToShow).commit();
-    }
-
-    protected void ShowCatDetailFragmentOverCatMasterFragment(String selectedCatID)
-    {
-        MyCatsDetailedFragment newFragmentToShow = new MyCatsDetailedFragment();
-        Bundle params = new Bundle();
-        params.putString("CatID", selectedCatID);
-        newFragmentToShow.setArguments(params);
-        getSupportFragmentManager()
-                .beginTransaction()
-                // .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right)   // animation
-                .replace(R.id.frameLayout_mainMyCat, newFragmentToShow)
-                .addToBackStack(null)
-                .commit();
-    }
-
-    @Override
-    public void onCatSelected(MyCatsDataModel cats) {
-
-        ShowCatDetailFragmentOverCatMasterFragment(cats.getCatID());
-
-    }
-
 }
